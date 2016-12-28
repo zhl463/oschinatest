c18d1a63de6df3f38e7ab459cfa919d8e8e89549
zhl463 <hl6689513>
2016-12-28 15:07:19 +0800

    python match local pc

--- app/run2.py
+++ app/run2.py
@@ -7,8 +7,8 @@ os.system("adb uninstall net.oschina.app")
 os.system("adb uninstall net.oschina.app.test")
 os.system("adb shell rm -f /sdcard/report.xml")
 os.system("adb shell rm -f /sdcard/crash.txt")
-if os.path.exists("report.xml"):
-	os.remove("report.xml")
+# if os.path.exists("report.xml"):
+# 	os.remove("report.xml")
 os.system("adb install .\\build\\outputs\\apk\\debug\\osc-android-v2.9.9(1609281026)-oschina-debug.apk")	
 os.system("adb install .\\build\\outputs\\apk\\app-oschina-debug-androidTest-unaligned.apk")	
 exe_str = "adb shell am instrument -w -e reportDir sdcard -e reportFile report.xml net.oschina.app.test/net.oschina.app.Runner2"
